80ba883738b548418ebe4f009d0ce20e519b07f9
Gino Corrales <ginocorrales@users.noreply.github.com>
2021-12-07 10:31:10 -0600

    Couple rings

--- main.ts
+++ main.ts
@@ -1,6 +1,17 @@
-let ring = [0, 1, 0]
+let ring = neopixel.create(DigitalPin.P2, 24, NeoPixelMode.RGB)
 basic.forever(function () {
-    for (let index = 0; index <= 4; index++) {
-    	
+    for (let index = 0; index <= 23; index++) {
+        ring.setPixelColor(index, neopixel.colors(NeoPixelColors.Red))
+        ring.show()
+        basic.pause(50)
+        ring.setPixelColor(index, neopixel.colors(NeoPixelColors.Yellow))
+        ring.show()
+    }
+    for (let index = 0; index <= 23; index++) {
+        ring.setPixelColor(index, neopixel.colors(NeoPixelColors.Blue))
+        ring.show()
+        basic.pause(5)
+        ring.setPixelColor(index, neopixel.colors(NeoPixelColors.White))
+        ring.show()
     }
 })
